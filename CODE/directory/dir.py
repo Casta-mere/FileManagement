44d875ef4f36b269f99af39e2bf60f55df865e35
Casta-mere <castamerego@gmail.com>
2022-12-26 17:57:49 +0800

    chmod完成

--- CODE/directory/dir.py
+++ CODE/directory/dir.py
@@ -1,53 +1,109 @@
 import os
-
+import json
 rootpath = r"D:\Onedrive\OneDrive - txsxcy\Project\FileManagement"
+
+
 class MyDirectory:
-    def __init__(self,path,parent=None):
+    def __init__(self, path, parent=None):
         self.path = path
-        self.pathname=path.replace(rootpath,'home')
-        self.dirname= path.split('/')[-1]
+        self.pathname = path.replace(rootpath, 'home')
+        self.dirname = path.split('/')[-1]
+
         self.subdirname = []
-        self.subdir=[]
-        self.files=[]
-        self.parent=parent
+        self.subdir = []
+
+        self.filesname = []
+        self.files = []
+
+        self.parent = parent
+
         temp = os.listdir(self.path)
         for i in temp:
-            if(i.find('.')==-1):
+            if(i.find('.') == -1):
                 self.subdirname.append(i)
-                self.subdir.append(MyDirectory(self.path+'/'+i,self))
-            else:
-                self.files.append(i)
+                self.subdir.append(MyDirectory(self.path+'/'+i, self))
+            elif(i == '.git'):
+                self.filesname.append(i)
+                self.files.append(MyFile(self.path+'/'+i, self))
+            elif(not i.startswith('.')):
+                self.filesname.append(i)
+                self.files.append(MyFile(self.path+'/'+i, self))
 
     def get_files(self):
-        return self.files
+        return self.filesname
 
     def get_subdirname(self):
         return self.subdirname
 
+    def get_filename(self):
+        return self.filesname
+
     def get_parent(self):
         return self.parent
 
-    def tree(self,level=0):
-        if level==0:
+    def tree(self, level=0):
+        if level == 0:
             print(f"\033[34m{self.pathname}\033[0m")
         else:
             print('   '*level+f"\033[34m{self.dirname}\033[0m")
-        for i in self.files:
+        for i in self.filesname:
             print('   '*(level+1)+i)
         for i in self.subdir:
             i.tree(level+1)
 
-    def mkdir(self,dirname):
+    def mkdir(self, dirname):
         os.mkdir(self.path+'/'+dirname)
         self.subdirname.append(dirname)
-        self.subdir.append(MyDirectory(self.path+'/'+dirname,self))
-    
-    def rmdir(self,dirname):
+        self.subdir.append(MyDirectory(self.path+'/'+dirname, self))
+
+    def rmdir(self, dirname):
         self.subdir.remove(self.subdir[self.get_subdirname().index(dirname)])
         self.subdirname.remove(dirname)
         os.rmdir(self.path+'/'+dirname)
 
-
-
     def get_pathname(self):
         return self.pathname
+
+    def chmod(self, username, filename, p):
+            if(filename not  in self.filesname):
+                return "No such file or directory"
+            if(username!="root" and username != self.files[self.get_filename().index(filename)].author):
+                return "Permission denied"
+            self.files[self.get_filename().index(filename)].chmod(p)
+
+class MyFile:
+    def __init__(self, path, parent=None):
+        self.path = path
+        self.filename = path.split('/')[-1]
+        self.parent = parent
+        self.permissionsdir = f"{path[:-1*len(self.filename)]}.{self.filename}.json"
+        self.author,self.p1,self.p2=self.get_p()
+
+    def get_p(self):
+        if os.path.exists(self.permissionsdir):
+            pass
+        else:
+            with open(self.permissionsdir, 'w') as f:
+                f.write('{"author":"root","p1": "7", "p2": "7"}')
+
+        j=json.load(open(self.permissionsdir))
+        return j["author"], j["p1"], j["p2"]
+
+    def rm(self):
+        try:
+            os.remove(self.path)
+            os.remove(self.permissionsdir)
+        except:
+            pass
+    
+    def chmod(self,p):
+        self.p1=p[0]
+        self.p2=p[1]
+        with open(self.permissionsdir, 'w') as f:
+            dict={"author":self.author,"p1": self.p1, "p2": self.p2}
+            json.dump(dict,f)
+
+
+# d = MyDirectory(os.path.abspath('.'))
+# for i in d.files:
+#     i.get_p()
